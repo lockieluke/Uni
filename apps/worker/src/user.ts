71953e046a26818a0f2ec57d51c68a8bc0913a94
Sherlock Luk <sherlockluk@outlook.com>
2025-11-05 22:39:31 +0000

    Implement error handling for getTier

--- apps/worker/src/user.ts
+++ apps/worker/src/user.ts
@@ -7,20 +7,25 @@ import { Database } from "./database.types";
 import { THono } from "./types";
 import { getUsage } from "./usage";
 import { withMsgpack } from "./utils";
-import { UniMonthlyLimits, getTierById } from "@uni/api";
+import { UniMonthlyLimits, UniTiers, getTierById } from "@uni/api";
 
 const userRouter = new Hono<THono>();
 
-export async function getTier(c: Context<THono>) {
+export async function getTier(c: Context<THono>): Promise<typeof UniTiers[keyof typeof UniTiers]> {
   const user = c.get("user");
   const supabase = c.get("supabase");
 
   const { data: tier, error } = await supabase.rpc("get_tier", {
     user_id: user.id
   });
+
   if (error)
     throw new Error(`Failed to get user tier: ${error.message}`);
 
+  if (_.isNullish(tier) || !(Object.values(UniTiers) as number[]).includes(tier))
+    throw new Error(`Invalid tier value: ${tier}`);
+
+  // @ts-expect-error "tier" casting to relevant tier type
   return tier ?? 0;
 }
 
