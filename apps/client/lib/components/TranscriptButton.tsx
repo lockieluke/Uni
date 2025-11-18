d9e2c871edfc6d4ed05df9a71b712d0645117bbc
Sherlock Luk <sherlockluk@outlook.com>
2025-11-18 21:51:11 +0000

    Fix TranscriptButton animation issue

--- apps/client/lib/components/TranscriptButton.tsx
+++ apps/client/lib/components/TranscriptButton.tsx
@@ -4,40 +4,33 @@ import { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reani
 import AnimatedTouchable from "./AnimatedTouchable";
 
 export default function TranscriptButton({ className, onPressIn, onPressOut }: {
-    className?: string;
-    onPressIn?: () => void;
-    onPressOut?: () => void;
+  className?: string;
+  onPressIn?: () => void;
+  onPressOut?: () => void;
 }) {
-    const scale = useSharedValue(1);
+  const scale = useSharedValue(1);
 
-    const animatedStyle = useAnimatedStyle(() => {
-        return {
-            transform: [{ scale: scale.value }],
-        };
-    });
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ scale: withSpring(scale.value) }],
+    };
+  });
 
-    const handleScaleAnimation = (value: number) => {
-        scale.value = withSpring(value, {
-            damping: 5,
-            stiffness: 130,
-        });
-    }
-
-    return (
-        <AnimatedTouchable
-            activeOpacity={1}
-            onPressIn={() => {
-                handleScaleAnimation(0.9);
-                onPressIn?.();
-            }}
-            onPressOut={() => {
-                handleScaleAnimation(1);
-                onPressOut?.();
-            }}
-            style={animatedStyle}
-            className={cn("bg-purple-600 flex w-28 h-28 items-center justify-center rounded-full", className)}
-        >
-            <FontAwesome className={"text-center"} name="microphone" size={40} color="white" />
-        </AnimatedTouchable>
-    );
+  return (
+    <AnimatedTouchable
+      activeOpacity={1}
+      onPressIn={() => {
+        scale.value = 0.9;
+        onPressIn?.();
+      }}
+      onPressOut={() => {
+        scale.value = 1;
+        onPressOut?.();
+      }}
+      style={animatedStyle}
+      className={cn("bg-purple-600 flex w-28 h-28 items-center justify-center rounded-full", className)}
+    >
+      <FontAwesome className={"text-center"} name="microphone" size={40} color="white" />
+    </AnimatedTouchable>
+  );
 }
