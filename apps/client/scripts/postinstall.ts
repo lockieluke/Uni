95e7e66515bd9bd6df951594c5b6a3ccc0e456c6
Sherlock Luk <sherlockluk@outlook.com>
2025-11-19 19:39:04 +0000

    Lint fix node import

--- apps/client/scripts/postinstall.ts
+++ apps/client/scripts/postinstall.ts
@@ -1,5 +1,5 @@
+import * as path from "node:path";
 import { $ } from "bun";
-import * as path from "path";
 
 if (process.env.EAS_BUILD)
 	await $`bun build:all`.cwd(path.join(process.cwd(), "../.."));
